be93454c9c52bfffed460b4b5435ffb78221591b
lorenaprado <lorenaprado@users.noreply.github.com>
2020-09-26 13:44:31 -0300

    Create executeme.sh

---
+++ executeme.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+FECHA=`date`
+echo "Hello fellas, nice to see you here."
+echo "You need to save everything that you are gonna do in this console, so be prepare"
+echo "Create a file, named: cyf-<you github username>.txt with this content"
+echo "This file was created by: $USER on $FECHA"
+echo "and push it to this repo"
